062b1322ed71db0648d692b2be5f694b241bca05
Patrick Schmid <schmid@phys.ethz.ch>
2013-01-21 13:54:45 +0100

    add new Files to lib

---
+++ lib/BaNG/Common.pm
@@ -0,0 +1,17 @@
+package BaNG::Common;
+
+use Dancer ':syntax';
+
+use Exporter 'import';
+our @EXPORT = qw(
+    $prefix
+    $config_path
+    $config_global
+);
+
+our $prefix          = dirname($0);
+our $config_path     = "$prefix/etc";
+our $config_global   = "$config_path/bang_globals.yaml";
+
+
+1;
--- lib/BaNG/Routes.pm
+++ lib/BaNG/Routes.pm
@@ -1,8 +1,20 @@
 package BaNG::Routes;
 use Dancer ':syntax';
+use BaNG::Hosts;
+use BaNG::Common;
 
 get '/' => sub {
      template 'index' => {
-                 section => 'dashboard'
+              section => 'dashboard'
+     };
+};
+
+get '/hosts/list' => sub {
+
+     my @hosts = find_enabled_hosts("*_*.yaml", "$config_path/enabled" );
+
+     template 'hostslist' => {
+                  section => 'hostlist',
+                    hosts => \@hosts
      };
 };
