da98a449a2f30d915dbea963e175a0aad7c59646
Patrick Schmid <schmid@phys.ethz.ch>
2013-02-19 09:22:59 +0100

    fix fsinfo

--- lib/BaNG/Hosts.pm
+++ lib/BaNG/Hosts.pm
@@ -1,6 +1,7 @@
 package BaNG::Hosts;
 use Dancer ':syntax';
 use 5.010;
+use BaNG::Common;
 
 use Exporter 'import';
 our @EXPORT = qw(
@@ -11,7 +12,6 @@ our @EXPORT = qw(
 our %fsinfo;
 
 sub get_fsinfo {
-    my $i=0;
     my $df;
     my @mounts;
 
@@ -35,14 +35,30 @@ sub get_fsinfo {
             'filesystem' => $+{filesystem},
             'mount'      => $+{mountpt},
             'fstyp'      => $+{fstyp},
-            'blocks'     => $+{blocks},
-            'used'       => $+{used},
-            'available'  => $+{available},
+            'blocks'     => num2human($+{blocks}),
+            'used'       => num2human($+{used}*1024,1024),
+            'available'  => num2human($+{available}*1024,1024),
             'used_per'   => $+{usedper},
+            'css_class'  => check_fill_level($+{usedper}),
         };
     }
 
     return 1;
+}
+
+sub check_fill_level {
+    my ($level) = @_ ;
+    my $css_class = '';
+
+    if ( $level > 98 ){
+        $css_class = "alert_red";
+    } elsif ($level > 90) {
+        $css_class = "alert_orange";
+    } elsif ($level > 80) {
+        $css_class = "alert_yellow";
+    }
+
+    return $css_class;
 }
 
 1;
