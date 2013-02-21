f07eca64278f8435245a66ab9b01bb2d62780930
Patrick Schmid <schmid@phys.ethz.ch>
2013-02-21 10:00:53 +0100

    GetOptions: fix options

--- BaNG.pl
+++ BaNG.pl
@@ -6,10 +6,11 @@
 #
 # This is compiled with threading support
 #
-# 2013.02.20, Patrick Schmid <schmid@phys.ethz.ch>
+# 2013.02.20, Patrick Schmid <schmid@phys.ethz.ch> & Claude Becker <becker@phys.ethz.ch>
+#
 use strict;
 use warnings;
-use Getopt::Long;
+use Getopt::Long qw(:config no_auto_abbrev);
 use Cwd 'abs_path';
 use File::Basename;
 use lib dirname(abs_path($0))."/lib";
@@ -28,22 +29,23 @@ my $debug           = 1;
 my ($help, $showversion) = ('') x 2;
 my ($cfg_group, $bulk_type, $target_host, $nis_group) = ('') x 4;
 my $nthreads        = 1;
+my $wipe            = 0;
 
 
 #################################
 # Get/Check command options
 #
 GetOptions (
-    "h"     => \$help,
-    "v"     => \$showversion,
-    "d"     => \$debug,
-    "g=s"   => \$cfg_group,
-    "s=s"   => \$target_host,
-    "n=s"   => \$nis_group,
-    "th=i"  => \$nthreads,
+    "help"         => sub { usage('') },
+    "v|version"    => \$showversion,
+    "d|debug"      => \$debug,
+    "g|group=s"    => \$cfg_group,
+    "h|host=s"     => \$target_host,
+    "n|nis=s"      => \$nis_group,
+    "t|threads=i"  => \$nthreads,
+    "w|wipe"       => \$wipe,
 )
     or usage("Invalid commmand line options.");
-    usage('')                                   if ( $help );
     usage("Current version number: $version")   if ( $showversion );
     usage("You must provide some arguments")    unless ($target_host || $cfg_group || $nis_group || $showversion);
     usage("Number of threads must be positive") unless ($nthreads && $nthreads > 0);
@@ -75,20 +77,18 @@ sub usage {
         $message, qq(
         Usage Examples:
 
-        $command -s <hostname> -g <group>   # back up given host and group
-        $command -s <hostname>              # back up all groups of given host
+        $command -h <hostname> -g <group>   # back up given host and group
+        $command -h <hostname>              # back up all groups of given host
         $command -g <group>                 # back up all hosts of given group
-        $command -n <nisgroup>              # back up all paths defined by NIS group
+        $command -n <nisgroup>              # back up all hosts:/paths defined by NIS group
         $command -v                         # show version number
-        $command -h                         # show this help message
+        $command --help                     # show this help message
 
         Optional Arguments:
 
-        -th <nr>        # number of threads, default: 1
+        -t <nr>         # number of threads, default: 1
+        -w              # wipe the backup
         -d              # show debugging messages
-
-        Note: for activation of nis-clients, you must create a folder with the name of the host
-        example: mkdir /export/backup/data/workstations/<hostname>
     \n)
     );
     exit 0;
