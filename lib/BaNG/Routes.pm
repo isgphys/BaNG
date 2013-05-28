11b34499121d3044bb36725b46f9d67a5e8f0a04
Patrick Schmid <schmid@phys.ethz.ch>
2013-05-28 14:55:50 +0200

    remove Auth

--- lib/BaNG/Routes.pm
+++ lib/BaNG/Routes.pm
@@ -6,12 +6,10 @@ use BaNG::Route_Host;
 use BaNG::Route_Restore;
 use BaNG::Route_Statistics;
 use BaNG::Route_Schedule;
-#use BaNG::Authent;
 use BaNG::Common;
 use BaNG::Hosts;
 use BaNG::Config;
 use BaNG::Reporting;
-#use Auth::LDAP;
 
 prefix undef;
 
@@ -37,40 +35,3 @@ get '/bkpreport-overview' => sub {
     },{ layout => 0
     };
 };
-
-#hook 'before' => sub {
-#    if (! session('logged_in') && request->path_info !~ m{^/$}) {
-#        if (request->path_info !~ m{^/login}){
-#            session 'requested_path' => request->path_info;
-#            debug "before:".  request->path_info;
-#        }
-#    request->path_info('/login');
-#    }
-#};
-
-#get '/login' => sub {
-#    template 'login', {
-#        #   'err' => $err,
-#             'remotehost' => request->remote_host,
-#    };
-#};
-
-#post '/login' => sub {
-#    if (checkuser(params->{user}, params->{pass})) {
-#        session user => params->{user};
-#        session 'logged_in' => true;
-#        debug("Logged in successfully: " . params->{user});
-#        set_flash('You are logged in.');
-#        return redirect session 'requested_path' || '/';
-#        redirect '/';
-#    } else {
-#        redirect '/login?failed=1';
-#    }
-#};
-
-#get '/logout' => sub {
-#    session->destroy;
-#    debug("Logged out successfully");
-#    set_flash('You are logged out.');
-#    return redirect '/';
-#};
--- t/002_modules.t
+++ t/002_modules.t
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 
 # Install missing perl module
-# aptitude install libclone-perl libdancer-perl libdatetime-perl libdbi-perl libfile-find-rule-perl libforks-perl libjson-perl liblist-moreutils-perl liblocale-gettext-perl libmail-sendmail-perl libmodule-refresh-perl libtemplate-perl libthread-queue-perl libyaml-tiny-perl libmime-lite-perl libnet-ldap-perl
+# aptitude install libclone-perl libdancer-perl libdatetime-perl libdbi-perl libfile-find-rule-perl libforks-perl libjson-perl liblist-moreutils-perl liblocale-gettext-perl libmail-sendmail-perl libmodule-refresh-perl libtemplate-perl libthread-queue-perl libyaml-tiny-perl libmime-lite-perl
 
 require_ok 'Dancer';
 require_ok 'YAML::Tiny';
@@ -22,7 +22,6 @@ use_ok 'List::MoreUtils';
 use_ok 'List::Util';
 use_ok 'Mail::Sendmail';
 use_ok 'MIME::Lite';
-use_ok 'Net::LDAP';
 use_ok 'Net::Ping';
 use_ok 'POSIX';
 use_ok 'Template';
