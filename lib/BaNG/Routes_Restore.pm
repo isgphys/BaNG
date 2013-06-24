2d7a951cac94de7b04dfc68cd26ce216ffd6586f
Patrick Schmid <schmid@phys.ethz.ch>
2013-06-24 14:18:41 +0200

    add backup_stack to restore view

--- lib/BaNG/Routes_Restore.pm
+++ lib/BaNG/Routes_Restore.pm
@@ -2,6 +2,7 @@ package BaNG::Routes_Restore;
 
 use Dancer ':syntax';
 use BaNG::Config;
+use BaNG::Common;
 
 prefix '/restore';
 
@@ -9,11 +10,18 @@ get '/' => sub {
     get_serverconfig();
     get_host_config('*');
 
+    my %hosts_stack;
+    foreach my $hostgroup ( sort keys %hosts ) {
+        my $host = $hosts{$hostgroup}{hostname};
+        $hosts_stack{$host} = backup_folders_stack($host);
+    }
+
     template 'restore' => {
         section      => 'restore',
         remotehost   => request->remote_host,
         webDancerEnv => config->{run_env},
         hosts        => \%hosts,
+        backupstack  => \%hosts_stack,
     };
 };
 
