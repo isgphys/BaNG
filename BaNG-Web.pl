8f6c2a4e65c58aac474dd2d747c20b8d0bd35c72
Patrick Schmid <schmid@phys.ethz.ch>
2014-08-18 14:49:36 +0200

    BaNG-Web: fix config.yml check

--- BaNG-Web.pl
+++ BaNG-Web.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/env perl
 use lib '/opt/BaNG/lib';
+use Cwd qw( abs_path );
 use Dancer;
-
 use BaNG::Routes;
 
-if ( -e "config.yml" ) {
+my $prefix = dirname( abs_path($0) );
+
+if ( -e "$prefix/config.yml" ) {
     Dancer->dance;
 } else {
     print "Error config.yml missing!\n";
