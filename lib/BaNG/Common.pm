d6e8b9902b8c08199e7a9a78923b239cffae2257
Patrick Schmid <schmid@phys.ethz.ch>
2016-06-08 14:03:12 +0200

    Common.pm: move *2human functions to the new module Converter.pm

--- lib/BaNG/Common.pm
+++ lib/BaNG/Common.pm
@@ -14,10 +14,8 @@ our @EXPORT = qw(
     list_groups
     list_groupmembers
     get_automount_paths
-    num2human
     targetpath
     check_target_exists
-    time2human
 );
 
 sub targetpath {
@@ -130,42 +128,6 @@ sub get_automount_paths {
     }
 
     return \%automnt;
-}
-
-sub num2human {
-    my ( $num, $base ) = @_;
-    $base ||= 1000.;
-
-    # convert large numbers to K, M, G, T notation
-    foreach my $unit ( '', qw(k M G T P) ) {
-        if ( $num < $base ) {
-            if ( $num < 10 && $num > 0 ) {
-                return sprintf( '%.2f %s', $num, $unit );    # print small values with 1 decimal
-            } else {
-                return sprintf( '%.1f %s', $num, $unit );    # print larger values without decimals
-            }
-        }
-        $num = $num / $base;
-    }
-}
-
-sub time2human {
-    my ($minutes) = @_;
-
-    # convert large times in minutes to hours
-    if ( $minutes eq '-' ) {
-        return '-';
-    } else {
-        if ( $minutes < 60 ) {
-            if ( $minutes < 1 ) {
-                return sprintf( '< 1 min', $minutes );
-            } else {
-                return sprintf( '%d min', $minutes );
-            }
-        } else {
-            return sprintf( '%dh%02dmin', floor( $minutes / 60 ), $minutes % 60 );
-        }
-    }
 }
 
 1;
---
+++ lib/BaNG/Converter.pm
@@ -0,0 +1,52 @@
+
+package BaNG::Converter;
+
+use 5.010;
+use strict;
+use warnings;
+use Date::Parse;
+use POSIX qw( floor );
+
+use Exporter 'import';
+our @EXPORT = qw(
+    num2human
+    time2human
+);
+
+sub num2human {
+    my ( $num, $base ) = @_;
+    $base ||= 1000.;
+
+    # convert large numbers to K, M, G, T notation
+    foreach my $unit ( '', qw(k M G T P) ) {
+        if ( $num < $base ) {
+            if ( $num < 10 && $num > 0 ) {
+                return sprintf( '%.2f %s', $num, $unit );    # print small values with 1 decimal
+            } else {
+                return sprintf( '%.1f %s', $num, $unit );    # print larger values without decimals
+            }
+        }
+        $num = $num / $base;
+    }
+}
+
+sub time2human {
+    my ($minutes) = @_;
+
+    # convert large times in minutes to hours
+    if ( $minutes eq '-' ) {
+        return '-';
+    } else {
+        if ( $minutes < 60 ) {
+            if ( $minutes < 1 ) {
+                return sprintf( '< 1 min', $minutes );
+            } else {
+                return sprintf( '%d min', $minutes );
+            }
+        } else {
+            return sprintf( '%dh%02dmin', floor( $minutes / 60 ), $minutes % 60 );
+        }
+    }
+}
+
+1;
