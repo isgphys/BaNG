aedba3ce3fe8aa76fc5ca277abef8cc94a749037
Maciej Bonin <mbonin@phys.ethz.ch>
2020-04-07 22:33:00 +0200

    back to static paths with carton

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,3 @@
-((nil .
-        ((eval . (setq-local flycheck-perl-include-path (list "./lib" "../lib/" (concat (getenv "HOME") "/git-repos/bang/lib") (concat (getenv "HOME") "/perl5/lib/perl5")))))))
+;;((nil .
+;;        ((eval . (setq-local flycheck-perl-include-path (list "./lib" "../lib/" (concat (getenv "HOME") "/git-repos/bang/lib") (concat (getenv "HOME") "/perl5/lib/perl5")))))))
+((perl-mode (flycheck-perl-include-path . ("./lib" "../lib" "../../lib" "./local/lib/perl5" "../local/lib/perl5" "../../local/lib/perl5"))))
