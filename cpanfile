8d36296316feb63fa8be49bbdd322aebca7b15d6
Maciej Bonin <mbonin@phys.ethz.ch>
2020-04-07 22:27:22 +0200

    fix access to hosts via ref, add placeholder for run_..._threads

---
+++ cpanfile
@@ -0,0 +1,6 @@
+requires 'YAML::Tiny';
+requires 'Text::Diff';
+requires 'DBI';
+requires 'MIME::Lite';
+requires 'Template';
+requires 'forks';
--- lib/BaNG/TM_lftp.pm
+++ lib/BaNG/TM_lftp.pm
@@ -26,7 +26,8 @@ sub queue_lftp_backup {
     my ( $startstamp, $endstamp );
     logit ($taskid, $host, $group, "Queueing lftp backup for host $host, group $group");
     my $bkptimestamp = eval_bkptimestamp ($host, $group);
-    my (@src_folders) = split( / /, $hostsref{$host-$group}->{hostconfig}->{BKP_SOURCE_FOLDER});
+    my (@src_folders) = split( / /, %$hostsref{$host-$group}->{hostconfig}->{BKP_SOURCE_FOLDER});
+
     # queue list of source folders as a whole
     $jobid = create_timeid( $taskid, $host, $group );
     my $bkpjob = {
@@ -47,4 +48,9 @@ sub queue_lftp_backup {
     
     logit( $taskid, $host, $group, "End of queueing backup of host $host group $group" );
     return $queueref;
+}
+
+sub run_lftp_threads {
+
+    return 0;
 }
