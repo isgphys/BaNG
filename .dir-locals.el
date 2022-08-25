a7f35de5bd5506b1b533a830930132626b782416
Claude Becker <becker@phys.ethz.ch>
2022-08-25 07:44:57 +0200

    rm dir-locals emacs config

--- .dir-locals.el
+++
@@ -1,3 +0,0 @@
-;;((nil .
-;;        ((eval . (setq-local flycheck-perl-include-path (list "./lib" "../lib/" (concat (getenv "HOME") "/git-repos/bang/lib") (concat (getenv "HOME") "/perl5/lib/perl5")))))))
-((perl-mode (flycheck-perl-include-path . ("./lib" "../lib" "../../lib" "./local/lib/perl5" "../local/lib/perl5" "../../local/lib/perl5"))))
